c66be562a99bc75859370cc5029f7a8245397683
Henok <henokmicrometre@gmail.com>
2022-03-27 09:04:03 +0100

    sorting navigtions

---
+++ components/Hero.jsx
@@ -0,0 +1,144 @@
+import Link from 'next/link'
+import Image from 'next/image'
+
+
+// styles
+const pageStyles = {
+  color: "#232129",
+  padding: 96,
+  fontFamily: "-apple-system, Roboto, sans-serif, serif",
+};
+const headingStyles = {
+  marginTop: 0,
+  marginBottom: 64,
+  maxWidth: 320,
+};
+const headingAccentStyles = {
+  color: "#663399",
+};
+const paragraphStyles = {
+  marginBottom: 48,
+};
+const codeStyles = {
+  color: "#8A6534",
+  padding: 4,
+  backgroundColor: "#FFF4DB",
+  fontSize: "1.25rem",
+  borderRadius: 4,
+};
+const listStyles = {
+  marginBottom: 96,
+  paddingLeft: 0,
+};
+const listItemStyles = {
+  fontWeight: 300,
+  fontSize: 24,
+  maxWidth: 560,
+  marginBottom: 30,
+};
+
+const linkStyle = {
+  color: "#8954A8",
+  fontWeight: "bold",
+  fontSize: 16,
+  verticalAlign: "5%",
+};
+
+const docLinkStyle = {
+  ...linkStyle,
+  listStyleType: "none",
+  marginBottom: 24,
+};
+
+const descriptionStyle = {
+  color: "#232129",
+  fontSize: 14,
+  marginTop: 10,
+  marginBottom: 0,
+  lineHeight: 1.25,
+};
+
+const docLink = {
+  text: "Documentation",
+  url: "https://www.gatsbyjs.com/docs/",
+  color: "#8954A8",
+};
+
+const badgeStyle = {
+  color: "#fff",
+  backgroundColor: "#088413",
+  border: "1px solid #088413",
+  fontSize: 11,
+  fontWeight: "bold",
+  letterSpacing: 1,
+  borderRadius: 4,
+  padding: "4px 6px",
+  display: "inline-block",
+  position: "relative",
+  top: -2,
+  marginLeft: 10,
+  lineHeight: 1,
+};
+
+// data
+const links = [
+  {
+    text: "MicrometreUK",
+    url: "/",
+    description:
+      "Brand Logo.",
+    color: "#563d7c",
+  },
+  {
+    text: "MicrometreUK Github repositories",
+    url: "https://github.com/micrometreuk",
+    description:
+      "projects/repos hosted on github",
+    color: "#1099A8",
+  },
+  {
+    text: "Find us on Facebook",
+    url: "https://www.facebook.com/micro.metre.9",
+    description:
+      "projects comminity group",
+    color: "#BC027F",
+  },
+];
+
+// markup
+const IndexPage = () => {
+  return (
+    <main style={pageStyles}>
+      <ul style={listStyles}>
+        <li style={docLinkStyle}>
+          <a
+            style={linkStyle}
+            href={`${docLink.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter`}
+          >
+            {docLink.text}
+          </a>
+        </li>
+        {links.map((link) => (
+          <li key={link.url} style={{ ...listItemStyles, color: link.color }}>
+            <span>
+              <a
+                style={linkStyle}
+                href={`${link.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter`}
+              >
+                {link.text}
+              </a>
+              {link.badge && (
+                <span style={badgeStyle} aria-label="New Badge">
+                  NEW!
+                </span>
+              )}
+              <p style={descriptionStyle}>{link.description}</p>
+            </span>
+          </li>
+        ))}
+      </ul>
+    </main>
+  );
+};
+
+export default IndexPage;
--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,19 +1,19 @@
 import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
-import Navbar from "./Navbar"
-import Footer from "./Footer"
+import Navbar from "./Navbar";
+import Footer from "./Footer";
 const name = "MicrometreUK";
 export const siteTitle = "data capture forms";
 
 export default function Layout({ children, home }) {
   return (
-        <div className="section">
+    <div className="section">
       <Head>
         <link rel="icon" href="/favicon.ico" />
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-  <meta name="theme-color" content="#ffffff"/>
+        <meta name="theme-color" content="#ffffff" />
         <meta
           name="description"
           content="Professional Painters and Decorators in Elmbridge and surrounding areas."
@@ -25,14 +25,14 @@ export default function Layout({ children, home }) {
         />
         <title>{siteTitle}</title>
       </Head>
-          <Navbar />
+      <Navbar />
       <main>{children}</main>
 
-          <Footer />
-          <style jsx>{`
+      <Footer />
+      <style jsx>{`
         .main {
-          margin: 0 auto;
-          padding: 1.5rem;
+          margin: 0;
+          padding: 0;
         }
       `}</style>
       <style jsx global>{`
@@ -40,28 +40,14 @@ export default function Layout({ children, home }) {
           margin: 0;
           padding: 0;
         }
-        :root {
-          --site-color: royalblue;
-          --divider-color: rgba(0, 0, 0, 0.4);
-        }
-        html {
-          font: 100%/1.5 system-ui;
-        }
+
         a {
           color: inherit;
-          text-decoration-color: var(--divider-color);
-          text-decoration-thickness: 2px;
+          text-decoration: none;
         }
-        a:hover {
-          color: var(--site-color);
-          text-decoration-color: currentcolor;
-        }
-        h1,
-        p {
-          margin-bottom: 1.5rem;
-        }
-        code {
-          font-family: 'Menlo';
+
+        ul {
+          list-style: none;
         }
       `}</style>
     </div>
